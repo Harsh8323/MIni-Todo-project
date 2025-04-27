6157a73041aa63ddb3686cd4d50cba15f7d4ba87
HrH_DeV <144415418+Harsh8323@users.noreply.github.com>
2025-04-27 23:33:50 +0530

    updates

--- script.js
+++ script.js
@@ -3,8 +3,5 @@ document.addEventListener("DOMContentLoaded", function () {
   const taskInput = document.getElementById("task-input");
   const addTaskButton = document.getElementById("addTaskButton");
   const todoList = document.getElementById("todo-List");
-
-  const allTaskButton = document.getElementById("allTaskButton");
-  const completedTaskButton = document.getElementById("completedTaskButton");
-  const pendingTaskButton = document.getElementById("pendingTaskButton");
+  const filterBtn = document.getElementById("filter-btn");
 });
